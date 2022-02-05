97c3c2ae8d2580f67560ad705019c8f18dafe29b
otakekazu <otake_guitar1213@icloud.com>
2022-02-05 15:54:48 +0900

    見た目を修正

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import Main from "./Main";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">焼肉ファインダー</header>
-      <Main />
+    <div className="app">
+      <header className="header">焼肉ファインダー</header>
+      <div className="main">
+        <Main />
+      </div>
+      <div className="description">
+        <p className="shortDesc">【使い方】</p>
+        <ul className="descList">
+          <li>起動するとブラウザが立ち上がり、マップが表示されます。</li>
+          <li>任意の場所をクリックすることで周辺の焼肉屋を検索できます。</li>
+          <li>最大で20件まで表示されます。</li>
+          <li>近くに焼肉屋が存在しない場合はアラートが表示されます。</li>
+        </ul>
+      </div>
     </div>
   );
 }
