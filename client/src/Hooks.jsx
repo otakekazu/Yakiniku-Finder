87096274a318a4e0b1b5a531eeae09e854b6fb82
otakekazu <otake_guitar1213@icloud.com>
2022-02-10 20:25:48 +0900

    カスタムフック・パフォーマンス改善

--- client/src/Hooks.jsx
+++ client/src/Hooks.jsx
@@ -1,23 +1,49 @@
-export const searchRestaurant = (e, map, setCenter, setMark, setIsLoading) => {
-  let service = new window.google.maps.places.PlacesService(map);
-  let slctPlace = new window.google.maps.LatLng(e.latLng.lat(), e.latLng.lng());
-  let request = {
-    location: slctPlace,
-    radius: "500",
-    type: ["restaurant"],
-    keyword: "yakiniku",
-  };
-  setCenter({
-    lat: slctPlace.lat(),
-    lng: slctPlace.lng()
-  })
-  service.nearbySearch(request, (results, status) => {
-    if (status == "OK") {
-      setMark(results);
-    } else {
-      alert("近くに焼肉屋はありません。");
-    }
-    setIsLoading(false);
+import { useState, useEffect } from "react";
+
+const useMap = () => {
+  const [map, setMap] = useState();
+  const [center, setCenter] = useState({
+    lat: 35.6809591,
+    lng: 139.7673068,
   });
-  return;
+  const [isLoading, setIsLoading] = useState(true);
+  const [marks, setMarks] = useState({});
+  const [service, setService] = useState();
+
+  useEffect(() => {
+    map && map.panTo(center);
+  }, [center]);
+
+  useEffect(() => {
+    map && setService(new window.google.maps.places.PlacesService(map));
+  }, [map]);
+
+  const searchRestaurant = (e) => {
+    setIsLoading(true);
+    let slctPlace = new window.google.maps.LatLng(
+      e.latLng.lat(),
+      e.latLng.lng()
+    );
+    let request = {
+      location: slctPlace,
+      radius: "500",
+      type: ["restaurant"],
+      keyword: "yakiniku",
+    };
+    setCenter({
+      lat: slctPlace.lat(),
+      lng: slctPlace.lng(),
+    });
+    service.nearbySearch(request, (results, status) => {
+      if (status == "OK") {
+        setMarks(results);
+      } else {
+        alert("近くに焼肉屋はありません。");
+      }
+      setIsLoading(false);
+    });
+  };
+  return [setMap, center, isLoading, setIsLoading, marks, searchRestaurant];
 };
+
+export default useMap;
--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,30 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { LoadScript, GoogleMap, Marker } from "@react-google-maps/api";
 import "./Main.scss";
-import { searchRestaurant } from "./Hooks";
+import useMap from "./Hooks";
 
 function Main() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [map, setMap] = useState();
-  const [mark, setMark] = useState({});
-  const [center, setCenter] = useState({
-    lat: 35.6809591,
-    lng: 139.7673068,
-  });
+  const [setMap, center, isLoading, setIsLoading, marks, searchRestaurant] =
+    useMap();
 
   const mapStyle = {
     width: "1000px",
     height: "500px",
   };
 
-  useEffect(() => {
-    map && map.panTo(center);
-  }, [center]);
-
   const onClick = (e) => {
     if (!e.placeId) {
-      setIsLoading(true);
-      searchRestaurant(e, map, setCenter, setMark, setIsLoading);
+      searchRestaurant(e);
     }
   };
 
@@ -44,15 +34,15 @@ function Main() {
           }}
           onClick={onClick}
         >
-          {Object.keys(mark).map((key) => {
+          {Object.keys(marks).map((key) => {
             return (
               <Marker
                 key={key}
                 position={{
-                  lat: mark[key].geometry.location.lat(),
-                  lng: mark[key].geometry.location.lng(),
+                  lat: marks[key].geometry.location.lat(),
+                  lng: marks[key].geometry.location.lng(),
                 }}
-                label={mark[key].name}
+                label={marks[key].name}
               />
             );
           })}
